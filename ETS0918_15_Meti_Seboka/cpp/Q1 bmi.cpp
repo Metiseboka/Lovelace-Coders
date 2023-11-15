0ede58378f0c809ea39a70a31788c46448bb196f
Metiseboka <149230080+Metiseboka@users.noreply.github.com>
2023-11-14 22:47:41 -0800

    Create Q1 bmi.cpp

---
+++ ETS0918_15_Meti_Seboka/cpp/Q1 bmi.cpp
@@ -0,0 +1,11 @@
+include <iostream>
+int main(){
+double weight, height, BMI;
+     cout << " enter weight in kilograms: ";
+    cin >> weight;
+    cout << " enter your height in meters: ";
+    cin >> height;
+    BMI = weight / (height * height);//calculate BMI
+   cout << "the BMI is: " << BMI;
+return 0;
+}
